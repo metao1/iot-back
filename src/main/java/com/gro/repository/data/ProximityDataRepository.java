75be252b600ef668a692ec98b6996f3c83f0176b
Mehrdad Karami <mehrdadkarami@Mehrdads-MBP.fritz.box>
2019-10-08 00:53:02 +0200

    cached

--- src/main/java/com/gro/repository/data/ProximityDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gro.repository.data;
-
-import com.gro.model.rpicomponent.data.ProximityData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProximityDataRepository extends JpaRepository<ProximityData, Integer> {
-
-}
